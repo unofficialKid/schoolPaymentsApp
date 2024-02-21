30460309fc7bc36519edca94ec47e0fc5f037f98
unofficialKid <ppatrycjaciesla@gmail.com>
2024-02-21 22:03:11 +0100

    remove unused imports

--- src/main/java/com/schoolpayments/maintenance/PaymentsTrackApplication.java
+++ src/main/java/com/schoolpayments/maintenance/PaymentsTrackApplication.java
@@ -3,8 +3,6 @@ package com.schoolpayments.maintenance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PaymentsTrackApplication {
